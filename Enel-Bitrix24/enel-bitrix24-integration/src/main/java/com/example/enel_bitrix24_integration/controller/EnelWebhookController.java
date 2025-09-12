f9f176f80cec26c511e2ea5113baf8dbec0034f1
ChiaraG91 <149475150+ChiaraG91@users.noreply.github.com>
2025-09-12 11:48:35 +0200

    Configurazione applicazione e logging

--- Enel-Bitrix24/enel-bitrix24-integration/src/main/java/com/example/enel_bitrix24_integration/controller/EnelWebhookController.java
+++ Enel-Bitrix24/enel-bitrix24-integration/src/main/java/com/example/enel_bitrix24_integration/controller/EnelWebhookController.java
@@ -1,4 +1,8 @@
 package com.example.enel_bitrix24_integration.controller;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class EnelWebhookController {
+
+    private static final Logger logger = LoggerFactory.getLogger(EnelWebhookController.class);
 }
--- Enel-Bitrix24/enel-bitrix24-integration/src/main/java/com/example/enel_bitrix24_integration/service/Bitrix24Service.java
+++ Enel-Bitrix24/enel-bitrix24-integration/src/main/java/com/example/enel_bitrix24_integration/service/Bitrix24Service.java
@@ -1,4 +1,9 @@
 package com.example.enel_bitrix24_integration.service;
+import com.example.enel_bitrix24_integration.controller.EnelWebhookController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Bitrix24Service {
+
+    private static final Logger logger = LoggerFactory.getLogger(Bitrix24Service.class);
 }
