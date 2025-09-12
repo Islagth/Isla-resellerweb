ff2018d4fd07878378aeef51acc15a955e081e0c
ChiaraG91 <149475150+ChiaraG91@users.noreply.github.com>
2025-09-12 10:50:04 +0200

    Initial commit

---
+++ Enel-Bitrix24/enel-bitrix24-integration/src/main/java/com/example/enel_bitrix24_integration/EnelBitrix24IntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.example.enel_bitrix24_integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnelBitrix24IntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnelBitrix24IntegrationApplication.class, args);
+	}
+
+}
---
+++ Enel-Bitrix24/enel-bitrix24-integration/src/test/java/com/example/enel_bitrix24_integration/EnelBitrix24IntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.enel_bitrix24_integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnelBitrix24IntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
