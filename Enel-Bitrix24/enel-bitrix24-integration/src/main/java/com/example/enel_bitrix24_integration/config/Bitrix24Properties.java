258a60c1c926e438ba664684661db288c10ac14b
ChiaraG91 <149475150+ChiaraG91@users.noreply.github.com>
2025-09-17 23:40:49 +0200

    Definizione modelli dati (DTO)

--- Enel-Bitrix24/enel-bitrix24-integration/src/main/java/com/example/enel_bitrix24_integration/config/Bitrix24Properties.java
+++ Enel-Bitrix24/enel-bitrix24-integration/src/main/java/com/example/enel_bitrix24_integration/config/Bitrix24Properties.java
@@ -1,20 +1,14 @@
 package com.example.enel_bitrix24_integration.config;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 
+@Getter
+@Setter
+@Configuration
 @ConfigurationProperties(prefix = "bitrix24")
 public class Bitrix24Properties {
-
-    /**
-     * URL del webhook REST di Bitrix24.
-     */
     private String url;
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
 }
