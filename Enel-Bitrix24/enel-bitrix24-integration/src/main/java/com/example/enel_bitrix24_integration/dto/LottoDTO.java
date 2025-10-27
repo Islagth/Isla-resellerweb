8082d4d916ba7c5dc9eab9ac8c282d9fbdc3012b
ChiaraG91 <149475150+ChiaraG91@users.noreply.github.com>
2025-10-27 19:24:38 +0100

    Update LottoDTO.java

--- Enel-Bitrix24/enel-bitrix24-integration/src/main/java/com/example/enel_bitrix24_integration/dto/LottoDTO.java
+++ Enel-Bitrix24/enel-bitrix24-integration/src/main/java/com/example/enel_bitrix24_integration/dto/LottoDTO.java
@@ -6,11 +6,6 @@ import lombok.Data;
 public class LottoDTO {
 
     private String id_lotto;
-    private String tipologia_lotto;
-    private String data_lotto;
-    private int contatti;
-    private int anagrafiche;
-    private String id_campagna;
-    private boolean masked;
-    private Boolean slice_by_tag;
+    private Boolean masked;
+
 }
