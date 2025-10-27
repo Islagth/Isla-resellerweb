2f1ed8ff1e5e606ccc0cebf35d4684f4f2ff7ed3
ChiaraG91 <149475150+ChiaraG91@users.noreply.github.com>
2025-10-27 19:29:44 +0100

    Update LottoService.java

--- Enel-Bitrix24/enel-bitrix24-integration/src/main/java/com/example/enel_bitrix24_integration/service/LottoService.java
+++ Enel-Bitrix24/enel-bitrix24-integration/src/main/java/com/example/enel_bitrix24_integration/service/LottoService.java
@@ -12,9 +12,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 @Service
 public class LottoService {
