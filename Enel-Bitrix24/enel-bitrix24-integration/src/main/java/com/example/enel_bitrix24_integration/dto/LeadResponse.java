8a8eee25fa00e72a536f0980a516d6cc4606ef02
ChiaraG91 <149475150+ChiaraG91@users.noreply.github.com>
2025-11-05 14:46:30 +0100

    Update LeadResponse.java

--- Enel-Bitrix24/enel-bitrix24-integration/src/main/java/com/example/enel_bitrix24_integration/dto/LeadResponse.java
+++ Enel-Bitrix24/enel-bitrix24-integration/src/main/java/com/example/enel_bitrix24_integration/dto/LeadResponse.java
@@ -2,11 +2,34 @@ package com.example.enel_bitrix24_integration.dto;
 
 import lombok.Data;
 
-@Data
 public class LeadResponse {
 
     private boolean success;
     private Long workedId;
     private String message;
 
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public Long getWorkedId() {
+        return workedId;
+    }
+
+    public void setWorkedId(Long workedId) {
+        this.workedId = workedId;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
+
