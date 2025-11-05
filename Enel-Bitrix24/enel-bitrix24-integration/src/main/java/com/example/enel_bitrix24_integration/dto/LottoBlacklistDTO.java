4f33f28a5dedae13318a8ed200d8c3c07f577cf9
ChiaraG91 <149475150+ChiaraG91@users.noreply.github.com>
2025-11-05 14:48:40 +0100

    Update LottoBlacklistDTO.java

--- Enel-Bitrix24/enel-bitrix24-integration/src/main/java/com/example/enel_bitrix24_integration/dto/LottoBlacklistDTO.java
+++ Enel-Bitrix24/enel-bitrix24-integration/src/main/java/com/example/enel_bitrix24_integration/dto/LottoBlacklistDTO.java
@@ -2,7 +2,6 @@ package com.example.enel_bitrix24_integration.dto;
 
 import lombok.Data;
 
-@Data
 public class LottoBlacklistDTO {
 
     private long id;
@@ -10,4 +9,35 @@ public class LottoBlacklistDTO {
     private int size;
     private String confirmation_date;
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getCreation_date() {
+        return creation_date;
+    }
+
+    public void setCreation_date(String creation_date) {
+        this.creation_date = creation_date;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public String getConfirmation_date() {
+        return confirmation_date;
+    }
+
+    public void setConfirmation_date(String confirmation_date) {
+        this.confirmation_date = confirmation_date;
+    }
 }
