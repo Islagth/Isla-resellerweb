8f28e548dc7a24ccf3f34bf20682879d3371ec0b
ChiaraG91 <149475150+ChiaraG91@users.noreply.github.com>
2025-11-05 14:56:41 +0100

    Update EnelProperties.java

--- Enel-Bitrix24/enel-bitrix24-integration/src/main/java/com/example/enel_bitrix24_integration/config/EnelProperties.java
+++ Enel-Bitrix24/enel-bitrix24-integration/src/main/java/com/example/enel_bitrix24_integration/config/EnelProperties.java
@@ -4,8 +4,6 @@ import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
-@Getter
-@Setter
 @Configuration
 @ConfigurationProperties(prefix = "enel.api")
 public class EnelProperties {
@@ -15,4 +13,35 @@ public class EnelProperties {
     private String clientId;
     private String clientJwt;
 
+    public String getBaseUrl() {
+        return baseUrl;
+    }
+
+    public void setBaseUrl(String baseUrl) {
+        this.baseUrl = baseUrl;
+    }
+
+    public String getAuthUrl() {
+        return authUrl;
+    }
+
+    public void setAuthUrl(String authUrl) {
+        this.authUrl = authUrl;
+    }
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
+    }
+
+    public String getClientJwt() {
+        return clientJwt;
+    }
+
+    public void setClientJwt(String clientJwt) {
+        this.clientJwt = clientJwt;
+    }
 }
