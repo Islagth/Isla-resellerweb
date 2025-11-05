e4476412bbc2651ca66c3c05e17b7f4cd27e95f2
ChiaraG91 <149475150+ChiaraG91@users.noreply.github.com>
2025-11-05 14:16:11 +0100

    Update ActivityDTO.java

--- Enel-Bitrix24/enel-bitrix24-integration/src/main/java/com/example/enel_bitrix24_integration/dto/ActivityDTO.java
+++ Enel-Bitrix24/enel-bitrix24-integration/src/main/java/com/example/enel_bitrix24_integration/dto/ActivityDTO.java
@@ -3,7 +3,7 @@ package com.example.enel_bitrix24_integration.dto;
 import lombok.Data;
 import java.time.LocalDateTime;
 
-@Data
+
 public class ActivityDTO {
     private Long id;
     private Long ownerId;           // ID del deal o contatto
@@ -17,5 +17,102 @@ public class ActivityDTO {
     private String subject;
     private String status;
     private Long responsibleId;
+
+     public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(Long ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public Integer getOwnerTypeId() {
+        return ownerTypeId;
+    }
+
+    public void setOwnerTypeId(Integer ownerTypeId) {
+        this.ownerTypeId = ownerTypeId;
+    }
+
+    public String getTypeId() {
+        return typeId;
+    }
+
+    public void setTypeId(String typeId) {
+        this.typeId = typeId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalDateTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalDateTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalDateTime endTime) {
+        this.endTime = endTime;
+    }
+
+    public LocalDateTime getDateModify() {
+        return dateModify;
+    }
+
+    public void setDateModify(LocalDateTime dateModify) {
+        this.dateModify = dateModify;
+    }
+
+    public LocalDateTime getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(LocalDateTime deadline) {
+        this.deadline = deadline;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Long getResponsibleId() {
+        return responsibleId;
+    }
+
+    public void setResponsibleId(Long responsibleId) {
+        this.responsibleId = responsibleId;
+    }
 }
+
 
